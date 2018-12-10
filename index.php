35695373cd5bd1eab16f2450e54921f97977aa58
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2018-12-10 11:46:47 -0600

    test

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>Animated Checkboxes and Radio Buttons with SVG</title>
+		<meta name="description" content="Animated Checkboxes and Radio Buttons with SVG: Using SVG for adding some fancy 'check' animations to form inputs" />
+		<meta name="keywords" content="animated checkbox, svg, radio button, styled checkbox, css, pseudo element, form, animated svg" />
+		<meta name="author" content="Codrops" />
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<link rel="stylesheet" type="text/css" href="less/main.css" />
+		<script src="js/modernizr.custom.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<!-- Top Navigation -->
+			<section>
+			
+			
+				<form class="ac-custom ac-radio ac-fill" autocomplete="off">
+					<h2>¿Cuál de estos es tu RK favorito?</h2>
+					<ul>
+					    <li> <img src="img/a1.png" alt=""> </li>
+					    <li><img src="img/b1.png" alt=""></li>
+					    <li><img src="img/c1.png" alt=""></li>
+					</ul>
+					<ul>
+						<li><input id="r1" name="r1" type="radio"><label for="r1">&nbsp;</label></li>
+						<li><input id="r2" name="r1" type="radio"><label for="r2">&nbsp;</label></li>
+						<li><input id="r3" name="r1" type="radio"><label for="r3">&nbsp;</label></li>
+					</ul>
+					
+				</form>
+
+				
+           
+			</section>
+			
+		</div><!-- /container -->
+		<script src="js/svgcheckbx.js"></script>
+	</body>
+</html>
